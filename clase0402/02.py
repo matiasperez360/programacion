e3a0df357ef58561653a6ac8a8f834853c2264c7
matias <matias.perez130inacapmail.cl>
2026-04-02 16:44:45 -0300

    edad

---
+++ clase0402/02.py
@@ -0,0 +1,15 @@
+edad = ""
+
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    mayor = True
+else:
+    mayor = False
+
+if mayor:
+    print("Usted si es mayor de edad")
+else:
+    print("Usted no es mayor de edad")
+
+
